7f58c426ac046a1bdc597cce22846ed20f0fa980
Thilini Dilhara <dilhara085@gmail.com>
2025-01-08 00:35:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,72 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:programming_quiz/Directory/theme_notifier.dart';
+import 'package:provider/provider.dart';
 import 'package:programming_quiz/Services/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  // Enable offline persistence
+  FirebaseDatabase.instance.setPersistenceEnabled(true);
+  FirebaseDatabase.instance.setPersistenceCacheSizeBytes(10000000); // Optional: Set cache size to 10MB
+
+  runApp(
+    ChangeNotifierProvider<ThemeNotifier>(
+      create: (_) => ThemeNotifier(ThemeMode.system),
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Define Light Theme
+  final ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.blue,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFFDCC00A),
+      foregroundColor: Colors.black,
+    ),
+    scaffoldBackgroundColor: Colors.white,
+    cardColor: Colors.white,
+    iconTheme: const IconThemeData(color: Colors.black),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.black), // Updated from bodyText2
+    ),
+  );
+
+  // Define Dark Theme
+  final ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.blue,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.black,
+      foregroundColor: Colors.white,
+    ),
+    scaffoldBackgroundColor: Colors.grey[900],
+    cardColor: Colors.grey[800],
+    iconTheme: const IconThemeData(color: Colors.white),
+    textTheme: const TextTheme(
+      bodyMedium: TextStyle(color: Colors.white), // Updated from bodyText2
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      title: 'EduConnect',
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: themeNotifier.themeMode,
+      home: const SplashScreen(),
     );
   }
 }
