370ff786488e03d1f4945bdb89daf7336e706957
Thilini Dilhara <dilhara085@gmail.com>
2025-01-08 08:25:12 +0530

    Update theme_notifier.dart

--- lib/Directory/theme_notifier.dart
+++ lib/Directory/theme_notifier.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  ThemeMode _themeMode;
+
+  ThemeNotifier(this._themeMode);
+
+  ThemeMode get themeMode => _themeMode;
+
+  bool get isDarkMode => _themeMode == ThemeMode.dark;
+
+  void toggleTheme() {
+    if (_themeMode == ThemeMode.dark) {
+      _themeMode = ThemeMode.light;
+    } else {
+      _themeMode = ThemeMode.dark;
+    }
+    notifyListeners();
+  }
+
+  void setTheme(ThemeMode mode) {
+    _themeMode = mode;
+    notifyListeners();
+  }
+}
 
